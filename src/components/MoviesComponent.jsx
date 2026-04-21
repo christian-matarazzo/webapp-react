91afb9d00093f29f3f89771e75dda8cfe5ebfe26
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-04-21 15:30:37 +0200

    add .env.example and .map on the backend data in MoviesComponent

---
+++ src/components/MoviesComponent.jsx
@@ -0,0 +1,29 @@
+export default function MoviesComponent({ movies, backendUrl, backendUrlMovies }) {
+    return (
+        <>
+<div className="container py-5">
+    <div className="row g-4 "> 
+        {movies.map(movie => (
+            <div className="col-12 col-md-4 col-lg-3 d-flex justify-content-center" key={movie.id}>
+                <div className="card h-100" style={{ width: '18rem' }}>
+                    <img
+                        src={`${backendUrl}/static/${movie?.image}`}
+                        className="card-img-top"
+                        alt={movie?.title}
+                        style={{ height: '250px', objectFit: 'cover' }}
+                    />
+                    <div className="card-body d-flex flex-column">
+                        <h5 className="card-title">{movie?.title}</h5>
+                        <h6 className="card-subtitle mb-2 text-muted">{movie?.genre}</h6>
+                        <p className="card-text">
+                            {movie?.abstract}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        ))}
+    </div>
+</div>
+        </>
+    )
+}
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react"
+import MoviesComponent from "../components/MoviesComponent"
 export default function Movie() {
+
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        fetch(backendMoviesUrl)
+            .then(response => response.json())
+            .then(data => setMovies(data))
+    }, [])
+
+
+
+
+const backendMoviesUrl = import.meta.env.VITE_APP_BACKEND_URL_MOVIES
+const backendUrl = import.meta.env.VITE_APP_BACKEND_URL
     return (
         <main>
-            <div>
-                Sono la pagina dei film
-            </div>
+            <MoviesComponent movies = {movies} backendUrl = {backendUrl} backendMoviesUrl = {backendMoviesUrl}/>
         </main>
     )
 
